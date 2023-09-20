2d6ea0400a25f9f4310d4586b125664ad0a7fd00
manupadev-asc <manupa.samarawickrama@ascentic.se>
2023-09-20 19:41:14 +0530

    fix: update prod

--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import { Blog } from "@/assets/data/blogs";
 
 export const getBlogs = async () => {
-  const res = await fetch("http://localhost:3000/api/blogs");
+  const res = await fetch("https://nextjs-workshop-git-pt-3-data-fetching-manupadev.vercel.app/api/blogs");
   const blogs: Blog[] = await res.json();
   return blogs;
 };
