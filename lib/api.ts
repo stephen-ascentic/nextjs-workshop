b582f18044c67d742cb8f934b5976b5887c8be7f
Stephen Blom <stephen.blom@ascentic.se>
2023-11-02 03:14:20 +0530

    Deployment update

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,8 @@
 import { Blog } from '@/types/Blog';
+const JSON_SERVER_URL = process.env.JSON_SERVER_URL;
 
 export const getBlogs = async () => {
-  const res = await fetch('http://localhost:8000/blogs', { cache: 'no-store' });
-  // const res = await fetch("http://localhost:3000/api/blogs", { cache: "no-store" });
+  const res = await fetch(`${JSON_SERVER_URL}/blogs`, { cache: 'no-store' });
 
   const blogs: Blog[] = await res.json();
 
@@ -11,7 +11,7 @@ export const getBlogs = async () => {
 
 export const getBlog = async (id: string) => {
   //   const res = await fetch(`http://localhost:3000/api/blogs/${id}`, { cache: "no-store" });
-  const res = await fetch(`http://localhost:8000/blogs?id=${id}`);
+  const res = await fetch(`${JSON_SERVER_URL}/blogs?id=${id}`);
 
   if (!res.ok) {
     return undefined;
